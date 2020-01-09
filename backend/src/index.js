570211d46d8a81b16ce667ab88565275978d73e3
Marlon Calvo <mcalvo@knights.ucf.edu>
2020-01-09 21:35:53 +0000

    some more backend updates

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const db = require('./db')
+const contactRouter = require("./routes/contact-router")
 
 const app = express()
 const apiPort = 3000
@@ -16,5 +17,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.use('/api', contactRouter)
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
