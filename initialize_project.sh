46d4c49485aaabb021a82bb76649a0d03abafbdd
Your Name <you@example.com>
2020-01-14 06:16:37 +0000

    add user friendly init and start

--- initialize_project.sh
+++ initialize_project.sh
@@ -4,12 +4,51 @@ dirs=( "frontend" "backend" )
 
 for dir in "${dirs[@]}"
 do
-    pushd $dir
-    rm -rf node_modules > /dev/null
-    npm cache clean -f > /dev/null
-    ln -s node_modules /home/vagrant/do_not_touch/$dir
-    npm install --no-bin-links 
-    popd
-done
+	if [ -d "$dir" ]
+	then
+		echo "It seems that the \"$dir\" package has been initialized."
+		printf "Do you still want to continue [y/n] "
+		read resp
+		echo ""
+		if [ "$resp" != "y" ] && [ "$resp" != "Y" ]
+		then
+			echo "Skipping initialization for \"$dir\"."
+			echo ""
+			continue
+		fi
+	fi
 
-./setup_defaults.sh
+	echo "Starting initialization for \"$dir\"." 
+	echo "================================================================"
+    pushd $dir > /dev/null 2>&1
+    echo ""
+    echo "Removing old node_modules folder from $dir/node_modules and /home/vagrant/do_not_touch/$dir."
+    rm -rf node_modules /home/vagrant/do_not_touch/$dir > /dev/null
+    echo "Clearing npm cache."
+    npm cache clean -f > /dev/null 2>&1
+    ln -s node_modules /home/vagrant/do_not_touch/$dir
+    echo ""
+    echo "Starting installation for \"$dir\" packages."
+    echo ""
+    npm install --no-bin-links
+    echo ""
+    echo "Finished initialization for \"$dir\""
+	echo "================================================================"
+    echo ""
+    echo ""
+
+    popd > /dev/null 2>&1
+done
+echo "This project requires environment variables for running servers. They are loaded from .env files."
+printf  "Do you want to use default (recommended)? [y/n] "
+read resp
+echo ""
+if [ "$resp" != "y" ] && [ "$resp" != "Y" ]
+then
+	echo "Follow the examples under .env.examples for each .env to create the environment files."
+else
+	echo "Setting up default testing environment variables."
+	cd env_setup_scripts
+	./setup_defaults.sh
+	echo "Done. You can now use \"start_server.sh\" to run the servers, or start either server manually."
+fi
--- start_servers.sh
+++ start_servers.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-if [ ! -d "/path/to/dir" ] 
+if [ ! -d "frontend/node_modules" ] || [ ! -d "backend/node_modules" ] 
 then
 	echo "Project has not been initialized."
+	echo "If it already has, it has been improperly initialized and should be re-initialized."
+	printf "Would you like to initialize the project? [y/n] "
+	read resp
+	echo ""
+	if [ "$resp" != "y" ] && [ "$resp" != "Y" ]
+	then
+		echo "Ok, skipping initialization."
+		exit 0
+	fi
 	echo "Running initialization procedure."
 	./initialize_project.sh
 fi
