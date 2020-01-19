563a68ed5bbb94cd7cc520a5e2d25cab3c438f05
Marlon Calvo <marloncalv98@gmail.com>
2020-01-19 02:23:14 +0000

    sanitize backend index

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,26 +1,24 @@
+const express    = require('express')
 const bodyParser = require('body-parser')
-const express = require('express')
-const cors = require('cors')
+const cors       = require('cors')
+const logger     = require('morgan')
 
+// Initialize express.
 const app = express()
 app.use(cors());
-app.options('*', cors());  // enable pre-flight
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+// Initialize database.
 const db = require('./db')
-const contactRouter = require("./routes/contact-router")
-const userRouter = require("./routes/user-router")
-
-const defaultPort = 8080
-const apiPort = process.env.PORT || defaultPort
-
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
+// Setup routing.
+const userRouter = require("./routes/user-router")
 app.use('/api/user', userRouter)
+
+// Setup listener on port.
+const defaultPort = 8080
+const apiPort     = process.env.PORT || defaultPort
 
 app.listen(apiPort, () => console.log(` Backend server running on port ${apiPort}`))
