944e2b4129dc95b24f3de33eeb238e6fbbf2ca08
Marlon Calvo <marloncalv98@gmail.com>
2020-01-30 18:26:26 -0500

    merge fix (#17)
    * move console debug
    
    * move console debug2

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
 
 axios.get(`api/version`)
   .then((resp) => {
