df67fbfcd030a69ac362c47fac65d287673359b3
Marlon Calvo <marloncalv98@gmail.com>
2020-01-30 17:56:09 -0500

    CD integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+
+  axios.get(`api/version`)
+  .then((resp) => {
+    console.log("Version ok!" + resp.data)
+  })
+  .catch(error => {
+    console.log(`Axios error: ${error}`);
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. This is a test.
         </p>
         <a
           className="App-link"
