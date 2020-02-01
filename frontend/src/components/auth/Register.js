14b35556f1d2bb48e74e33a42014464cbfb89750
Marlon Calvo <marloncalvo@localhost.localdomain>
2020-02-01 14:51:42 -0500

    add routes for each view

---
+++ frontend/src/components/auth/Register.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Register extends Component {
+    render() {
+        return (
+            <div>
+                Register page!
+            </div>
+        );
+    }
+}
+
+export default Register;
