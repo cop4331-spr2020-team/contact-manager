950200a565dc76faf7da76219c67bb5a4b36852e
Jork <zeng.keanu@gmail.com>
2020-02-01 16:51:35 -0500

    Add files via upload

---
+++ frontend/src/LandingPage/LandingPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import './Home.css';
+import Home from "./Home";
+
+function LandingPage() {
+  return (
+    <div className="wrap">
+        <Home/>
+    </div>
+    
+  );
+}
+export default LandingPage;
