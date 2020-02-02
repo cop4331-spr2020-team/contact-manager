3553a74642cb0b2b61fc4f19d9be81dd93818776
Jork <zeng.keanu@gmail.com>
2020-02-02 00:39:48 -0500

    Add files via upload

--- frontend/src/LandingPage/Home.js
+++ frontend/src/LandingPage/Home.js
@@ -3,120 +3,119 @@ import { Jumbotron, Button, Container, CardTitle, CardText, Row, Col, Card, Card
 import { Link, Redirect } from "react-router-dom";
 
 import './Home.css';
-import aboutImg from '../qmark.jpg';
-import contactImg from '../notebook.jpg';
-import loginImg from '../signin.jpg';
+import aboutImg from './qmark.jpg';
+import contactImg from './notebook.jpg';
+import loginImg from './signin.jpg';
 
 function Home(props) {
-  
-  const [isLogin,setLogin]=useState(false); // Need to check token to toggle this
 
-  function loginCheck() {
+    const [isLogin,setLogin]=useState(false); // Need to check token to toggle this
+
+  function logout() {
     if (isLogin) {
       setLogin(false);
-                        // clear login token 
+                        // clear login token
     }
   }
 
   if (isLogin){ // Load this if the user is logged in
     return (
-      <Container>
-        
-        <Jumbotron>
-          <h1 className="display-3">Welcome!</h1>
-          <p className="lead">Organize the people around you.</p>
-          <hr className="my-2" />
-        </Jumbotron>
-       
+
+      <div>
+          {/*  Banner at the top of page */}
+        <div className="banner">
+          <div className="wrap3">
+            <h1 class="text-center" className="display-3">Contact Manager</h1>
+          </div>
+        </div>
+
         <div>
           <Row className="cardsrow">
            <Col sm="3">             {/* contacts page card*/}
-  
-              <CardImg top width="100%" src={contactImg} alt="Card image cap" className="cards"/> 
+
+              <CardImg top width="100%" src={contactImg} alt="Card image cap" className="cards"/>
 
               <Link to="/contacts"> {/* Route link to contacts page */}
-                <Card body className="cards">                 
+                <Card body className="cards">
                   <Button color="btn-lg btn-dark btn-block">Contacts</Button>
                 </Card>
 
               </Link>
-            </Col> 
-   
-            <Col sm="3">            {/* sign out card */}                    
+            </Col>
+
+            <Col sm="3">            {/* sign out card */}
               <CardImg top width="100%" src={loginImg} alt="Card image cap" className="cards"/>
-                    
+
               <Card body className="cards">
-                <Button color="btn-lg btn-dark btn-block" onClick={loginCheck}>{isLogin? 'Sign Out' : 'Sign In'}</Button>
+                <Button color="btn-lg btn-dark btn-block" onClick={logout}>Sign Out</Button>
               </Card>
             </Col>
-  
+
             <Col sm="3">              {/* about page card */}
-                      
+
               <CardImg top width="100%" src={aboutImg} alt="Card image cap" className="cards"/>
 
-              <Link to="/about">      {/* Route link to about page */}  
+              <Link to="/about">      {/* Route link to about page */}
                 <Card body className="cards">
                   <Button color="btn-lg btn-dark btn-block">About</Button>
                 </Card>
-  
+
               </Link>
             </Col>
-  
+
           </Row>
         </div>
-      </Container>
+      </div>
     )
   }
 
   else {  // If the user is not logged in, load this page
     return (
 
-      <Container>
-        
-        <Jumbotron>
-          <h1 className="display-3">Welcome!</h1>
-          <p className="lead">Organize the people around you.</p>
-          <hr className="my-2" />
-          <div className="text-center">
-
-            <Link to="/register">     {/* Route link to register page */} 
-              <Button outline color="secondary btn-lg">Sign Up Now</Button>
+      <div>
+          {/*  Banner at the top of page */}
+        <div className="banner">
+            <div className="wrap2">
+              <h1 class="text-center" className="display-3">Contact Manager</h1>
+            </div>
+          <div className="wrap1">
+            <h3 class="text-center">Sign up to manage your contacts now!</h3>
+            <Link to="/register" className="btn">      {/* Route link to register page */}
+              <Button color="btn-lg btn-dark btn-block">Sign Up!</Button>
             </Link>
-
           </div>
-        </Jumbotron>
-      
+        </div>
+
+
         <div>
           <Row className="cardsrow">
             <Col sm="3">                {/* login page card */}
-                                        
+
               <CardImg top width="100%" src={loginImg} alt="Card image cap" className="cards"/>
-              <Link to="/login">      {/* Route link to login page */}     
+              <Link to="/login">      {/* Route link to login page */}
                 <Card body className="cards">
-                  <Button color="btn-lg btn-dark btn-block" onClick={loginCheck}>{isLogin? 'Sign Out' : 'Sign In'}</Button>
+                  <Button color="btn-lg btn-dark btn-block">Sign In</Button>
                 </Card>
               </Link>
             </Col>
 
             <Col sm="3">              {/* about page card */}
-                   
 
-                <CardImg top width="100%" src={aboutImg} alt="Card image cap" className="cards"/>
-              <Link to="/about">      {/* Route link to about page */}     
-                  <Card body className="cards">
-                    <Button color="btn-lg btn-dark btn-block">About</Button>
-                  </Card>
+              <CardImg top width="100%" src={aboutImg} alt="Card image cap" className="cards"/>
+              <Link to="/about">      {/* Route link to about page */}
+                <Card body className="cards">
+                  <Button color="btn-lg btn-dark btn-block">About</Button>
+                </Card>
 
               </Link>
             </Col>
 
           </Row>
         </div>
-      </Container>
+      </div>
     );
   }
 }
 
+
 export default Home;
-
-
