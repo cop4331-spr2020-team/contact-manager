ff48caec78f745a158ff872c3e80bb3f182790f1
Marlon Calvo <marloncalvo@localhost.localdomain>
2020-02-03 03:56:44 -0500

    add 404 page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,14 +8,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Landing from './components/view/Landing';
 import LoginPage from './components/auth/LoginPage'
 import RegisterPage from './components/auth/RegisterPage'
+import NoMatch from './components/view/NoMatch'
 
 function App() {
   return (
     <Router>
     <div className="App">
-      <Route exact path = '/' component={Landing} />
-      <Route exact path = '/login' component={LoginPage} />
-      <Route exact path = '/register' component={RegisterPage} />
+      <Switch>
+        <Route exact path = '/' component={Landing} />
+        <Route exact path = '/login' component={LoginPage} />
+        <Route exact path = '/register' component={RegisterPage} />
+        <Route component={NoMatch} />
+      </Switch>
     </div>
     </Router>
   );
---
+++ frontend/src/components/view/NoMatch.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class NoMatch extends Component {
+    render() {
+        return (
+            <div>
+                Woops! The page you selected was not found. 404
+            </div>
+        );
+    }
+}
