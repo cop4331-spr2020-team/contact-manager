2e7b265ade6ddeac403d57b78c2ea737e014e9e3
Marlon Calvo <marloncalvo@localhost.localdomain>
2020-02-02 21:24:50 -0500

    add pagination for user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,7 @@ import React from 'react';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import AppNavbar from './components/AppNavbar';
-import ContactList from './components/ContactList';
-import ItemModal from './components/ItemModal';
-import SearchBar from './components/SearchBar';
-import { Container } from 'reactstrap';
-
-// Redux dependencies
-import { Provider } from 'react-redux';
-import store from './store';
-
-// ReactStrap dependencies
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 import './App.css';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Navbar from './components/layout/MainNavbar'
@@ -24,23 +10,20 @@ import Landing from './components/view/Landing';
 import LoginPage from './components/auth/LoginPage'
 import RegisterPage from './components/auth/RegisterPage'
 
-import Contact from './components/view/Contact'
+import ContactsView from './components/view/ContactsView'
 
 function App() {
   return (
-    <Provider store={store}>
     <Router>
     <div className="App">
       <Route exact path = '/' component={Landing} />
       <Route exact path = '/login' component={LoginPage} />
       <Route exact path = '/register' component={RegisterPage} />
 
-      <Route path = '/contact' component={Contact} />
-      <Route exact path = '/contacts' component={itemModal} />
+      <Route path = '/contact' component={ContactsView} />
 
     </div>
     </Router>
-    </Provider>
   );
 }
 
